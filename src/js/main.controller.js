65c8a3de6917a45761869fe5fa6bb65792b8d01f
Jonathan Wood <bamblehorse@gmail.com>
2017-02-08 01:59:20 +0000

    feature complete closes #11

--- src/js/main.controller.js
+++ src/js/main.controller.js
@@ -15,15 +15,24 @@ export default class Main {
     this.planets = [];
     this.count = 0;
 
+    this.search = '';
     this.swapi = Swapi;
     this.getPlanets();
   }
+  searchApi() {
+    if (this.search !== '') {
+      this.page = 0;
+      this.setMarker();
+      this.getPlanets('http://swapi.co/api/planets/?search=' + this.search);
+    }
+    else {
+      this.getPlanets();
+    }
+  }
 
   getPlanets (url='https://swapi.co/api/planets/?page=' + (this.page + 1)) {
-    console.log(url);
     this.swapi.getPlanets(url)
       .then((res) => {
-        console.log(res);
         this.count = res.count;
         let approxPages = this.count / 10;
         if (approxPages > Math.floor(approxPages)) {
@@ -59,6 +68,9 @@ export default class Main {
   }
 
   pageIs(page) {
+    if (this.pages === 0) {
+      return true;
+    }
     return this.page + 1 === page;
   }
 
@@ -83,8 +95,9 @@ export default class Main {
   }
 
   setMarker () {
+    const dots = '...';
     if (this.page > this.pages - 2) {
-      this.markerOne = '...';
+      this.markerOne = dots;
       this.markerTwo = this.page;
       this.markerThree = this.page + 1;
       if (this.page === this.pages) {
@@ -95,7 +108,12 @@ export default class Main {
     if (this.page < this.pages - 2) {
       this.markerOne = this.page + 1;
       this.markerTwo = this.page + 2;
-      this.markerThree = '...';
+      this.markerThree = dots;
+    }
+    if (this.pages < 2) {
+      this.markerOne = dots;
+      this.markerTwo = dots;
+      this.markerThree = dots;
     }
   }
 }
